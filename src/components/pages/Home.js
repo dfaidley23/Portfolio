33b42835e00039e94f217e2733534ed500ceea9c
David faidley <david.faidley23@gmail.com>
2022-05-21 14:57:46 -0600

    fixed wording

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -9,7 +9,7 @@ function Home(props) {
         </div>
         <div id="my-resume">
             <h1>My Resume</h1>
-            <p>For a copy of my resume please clink on the link below</p>
+            <p>For a copy of my resume please click on the link below</p>
             <a id="resume" href={props.resumeLink} target="_blank" rel="noopener noreferrer">My Resume!</a>
         </div>
     </section>
