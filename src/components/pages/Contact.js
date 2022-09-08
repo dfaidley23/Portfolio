5279e32bb74db6ca79f06ca92091f74c46bb950d
David faidley <david.faidley23@gmail.com>
2022-09-08 12:18:15 -0600

    changed contact section style

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,6 +1,6 @@
 export default function Contact(props) {
     return (
-      <div className='App-main'>
+      <div className='App-main' id='contact'>
         <h1>Contact Page</h1>
         <p>Thank You for viewing my Portfolio. If you wish to reach out to me for any inquiries then feel free to send me a message on LinkedIn. If you wish to follow me or for more looks into other examples of my work feel free to visit my Github.</p>
         <p><img id='linkedIn' src={props.linkIcon} alt='LinkedIn Icon'></img><a href={props.linkedIn}>LinkedIn</a></p>
